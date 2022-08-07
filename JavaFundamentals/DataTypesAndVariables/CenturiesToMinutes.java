7ec4282d00e62f4a915169f8fac7a340fcf9bfea
OlegMirchev <olegmirchev@gmail.com>
2022-08-07 08:32:00 +0300

    Added homework

---
+++ JavaFundamentals/DataTypesAndVariables/CenturiesToMinutes.java
@@ -0,0 +1,17 @@
+package JavaFundamentals2021.DataTypesLAB2605;
+
+import java.util.Scanner;
+
+public class CenturiesToMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int centuries = Integer.parseInt(scanner.nextLine());
+        int years = centuries * 100;
+        double days = years * 365.2422;
+        double hours = days * 24;
+        double minutes = hours * 60;
+
+        System.out.printf("%d centuries = %d years = %.0f days = %.0f hours = %.0f minutes%n" +
+                "", centuries, years, days, hours,minutes);
+    }
+}
