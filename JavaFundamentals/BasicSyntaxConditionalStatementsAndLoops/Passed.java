757a6d75389254d1712c7087ebbb8dfb939e8930
OlegMirchev <olegmirchev@gmail.com>
2022-08-09 20:14:54 +0300

    Added homework

---
+++ JavaFundamentals/BasicSyntaxConditionalStatementsAndLoops/Passed.java
@@ -0,0 +1,14 @@
+package JavaFundamentals2021.BasicSyntaxLAB1905;
+
+import java.util.Scanner;
+
+public class Passed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double input = Double.parseDouble(scanner.nextLine());
+
+        if (input >= 3) {
+            System.out.println("Passed!");
+        }
+    }
+}
