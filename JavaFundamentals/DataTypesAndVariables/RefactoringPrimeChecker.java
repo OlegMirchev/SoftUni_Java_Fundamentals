828ff6a8c8e946ccff74fcd95dd09a44447238ca
OlegMirchev <olegmirchev@gmail.com>
2022-08-28 08:23:22 +0300

    Added homework

---
+++ JavaFundamentals/DataTypesAndVariables/RefactoringPrimeChecker.java
@@ -0,0 +1,21 @@
+package JavaFundamentals2021.DataTypesMoreExersice;
+
+import java.util.Scanner;
+
+public class RefactoringPrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 2; i <= n; i++) {
+            boolean isValid = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isValid = false;
+                    break;
+                }
+            }
+            System.out.printf("%d -> %b%n", i, isValid);
+
+        }
+    }
+}
