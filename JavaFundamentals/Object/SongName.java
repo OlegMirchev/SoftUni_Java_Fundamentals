e7e4c492b4b39b3fae3cb667a6558bcc25c1718a
OlegMirchev <olegmirchev@gmail.com>
2022-09-22 11:28:22 +0300

    Added homework

---
+++ JavaFundamentals/Object/SongName.java
@@ -0,0 +1,27 @@
+package JavaFundamentals2021.ObjectLAB2306;
+
+public class SongName {
+    private String type;
+    private String name;
+
+    public SongName(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
