a2d90b9a64b086f2e7fb4a45bca9d984b92c78b9
OlegMirchev <olegmirchev@gmail.com>
2022-10-23 08:44:18 +0300

    Added homework

---
+++ JavaFundamentals/Exam/PlantDiscovery.java
@@ -0,0 +1,10 @@
+package JavaFundamentals2021.FinalExam;
+
+import java.util.Scanner;
+
+public class PlantDiscovery {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
